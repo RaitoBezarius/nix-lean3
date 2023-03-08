dedcab094e63f0f57d545c0980681fe47f8d9f09
Raito Bezarius <masterancpp@gmail.com>
2023-03-08 14:55:24 +0100

    ci: split into multiple small tasks

--- ci.nix
+++ ci.nix
@@ -9,40 +9,28 @@
 # then your CI will be able to build and cache only those packages for
 # which this is possible.
 
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { }, leanVersion ? null }:
 
 with builtins;
 let
-  isReserved = n: n == "lib" || n == "overlays" || n == "modules" || n == "callPackage";
-  isDerivation = p: isAttrs p && p ? type && p.type == "derivation";
   isBuildable = p: !(p.meta.broken or false) && p.meta.license.free or true;
   isCacheable = p: !(p.preferLocalBuild or false);
-  shouldRecurseForDerivations = p: isAttrs p && p.recurseForDerivations or false;
-
-  nameValuePair = n: v: { name = n; value = v; };
 
   concatMap = builtins.concatMap or (f: xs: concatLists (map f xs));
-
-  flattenPkgs = s:
-    let
-      f = p:
-        if shouldRecurseForDerivations p then flattenPkgs p
-        else if isDerivation p then [ p ]
-        else [ ];
-    in
-    concatMap f (attrValues s);
 
   outputsOf = p: map (o: p.${o}) p.outputs;
 
   nurAttrs = import ./default.nix { inherit pkgs; };
 
   nurPkgs =
-    flattenPkgs
-      (listToAttrs
-        (map (n: nameValuePair n nurAttrs.${n})
-          (filter (n: !isReserved n)
-            (attrNames nurAttrs))));
-
+    with nurAttrs; (if leanVersion != null then [
+      lean.${leanVersion}
+      lean.${leanVersion}.coreLibrary
+      lean.${leanVersion}.emscripten-bin
+      lean.${leanVersion}.webEditor
+      lean.${leanVersion}.debugOleans
+      lean.${leanVersion}.noOleanVersionCheck
+    ] else []);
 in
 rec {
   buildPkgs = filter isBuildable nurPkgs;
